09155a231442cb98fe0b604f98e428221740d128
engrbugs <engrbugs@gmail.com>
2021-08-07 21:01:31 -0700

    2021-08-07-2101

--- main.py
+++ main.py
@@ -11,18 +11,16 @@ import h5py
 
 def print_hi(name):
     import h5py
-    filename = "train_catvnoncat.h5"
+    filename = "test_catvnoncat.h5"
 
     with h5py.File(filename, "r") as f:
         # List all groups
         print("Keys: %s" % f.keys())
-        a_group_key = list(f.keys())[0]
-
         # Get the data
-        data = list(f[a_group_key])
+        header = list(f.keys())
         classes = f['list_classes']
-        train_set_x = f['train_set_x']
-        train_set_y = f['train_set_y']
+        train_set_x = f['test_set_x']
+        train_set_y = f['test_set_y']
         classes = np.array(classes)
         train_set_x = np.array(train_set_x)
         train_set_y = np.array(train_set_y)
@@ -33,6 +31,8 @@ def print_hi(name):
     plt.imshow(train_set_x[index])
     print("y = " + str(train_set_y[index]) + ", it's a '" + classes[np.squeeze(train_set_y[index])].decode(
         "utf-8") + "' picture.")
+    print("total number of " + str(header[1]) + ": " + str(train_set_x.shape[0]))
+    print("total number of " + str(header[2]) + ": " + str(train_set_y.shape[0]))
     plt.show()
 
 
